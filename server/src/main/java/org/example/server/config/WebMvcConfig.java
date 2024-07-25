2cc9fc49f8e75892b733b3d0a49c08285c7cdb70
JangHongJoon <wkdghdwns19969@gmail.com>
2024-07-26 02:03:02 +0900

    fix : CORS-문제 해결 시도 4차 - 환경변수 코드 직접 적용

--- server/src/main/java/org/example/server/config/WebMvcConfig.java
+++ server/src/main/java/org/example/server/config/WebMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Value("${app.cors.allowedOrigins}")
+    @Value("${APP_CORS_ALLOWED_ORIGINS}")
     private String[] allowedOrigins;
 
     @Override
